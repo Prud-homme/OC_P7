cc1221ec66d4a6fca012a879bc58db0b9c8a0aeb
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-01-15 19:08:58 +0100

    Update optimize.py

--- algorithms/optimized.py
+++ algorithms/optimized.py
@@ -1,70 +1,91 @@
 from __future__ import annotations
 
 
-def optimized_invest(
-    actions: list[tuple[str, int, int]],
-    investment_mix: list[list[int]],
-    profits: list[int],
+def obtain_possible_investment(
+    names: list[str],
+    costs: list[float],
+    profits: list[float],
     node: int,
-    wallet: int,
-    investment: list[int],
-    profit_obtained: int,
+    wallet: float,
+    profit_obtained: float,
+    investment_earnings: list[float],
+    shares_purchased: list[str],
+    all_investments: list[list[str]],
 ) -> None:
     """
     The function recursively calculates the set of investments whose expenditure
     does not exceed a maximum value as well as the associated profits.
     Returns these two data as a list.
     """
-    if node >= len(actions):
-        investment_mix.append(investment)
-        profits.append(profit_obtained)
+    if node >= len(names):
+        investment_earnings.append(profit_obtained)
+        all_investments.append(shares_purchased)
 
     else:
-        action_cost = actions[node][1]
+        share_cost = costs[node]
+        new_wallet = wallet - share_cost
+        if new_wallet >= 0:
+            new_profit_obtained = profit_obtained + share_cost * profits[node] / 100
 
-        if wallet - action_cost > 0:
-            action_profit = actions[node][2]
-            profit = action_cost * action_profit / 100
-            investment_1 = investment.copy()
-            investment_1.append(1)
-            optimized_invest(
-                actions,
-                investment_mix,
+            new_shares_purchased = shares_purchased.copy()
+            new_shares_purchased.append(names[node])
+
+            obtain_possible_investment(
+                names,
+                costs,
                 profits,
                 node + 1,
-                wallet - action_cost,
-                investment_1,
-                profit_obtained + profit,
+                new_wallet,
+                new_profit_obtained,
+                investment_earnings,
+                new_shares_purchased,
+                all_investments,
             )
 
-        investment_0 = investment.copy()
-        investment_0.append(0)
-        optimized_invest(
-            actions,
-            investment_mix,
+        obtain_possible_investment(
+            names,
+            costs,
             profits,
             node + 1,
             wallet,
-            investment_0,
             profit_obtained,
+            investment_earnings,
+            shares_purchased,
+            all_investments,
         )
 
 
-def run(
-    actions: list[tuple[str, int, int]], max_expenses: int
-) -> tuple[list[list[int]], list[int]]:
+def printable_result(
+    investment_earnings: list[float], all_investments: list[list[str]]
+) -> str:
+    """Generates a message displaying the stocks to buy to maximize profits"""
+    best_choice = investment_earnings.index(max(investment_earnings))
+
+    return f"""[Meilleur investissement] Gain de {round(investment_earnings[best_choice],2)}€ pour l'achat de:
+{', '.join(all_investments[best_choice])}"""
+
+
+def optimized(
+    names: list[str], costs: list[float], profits: list[float], wallet: float
+) -> str:
     """
     The function returns the list of possible investments and the associated profits
     according to an algorithm optimized by the use of recursion.
     """
-    profits = []
-    investment_mix = []
-
     node = 0
-    wallet = max_expenses
-    investment = []
     profit_obtained = 0
-    optimized_invest(
-        actions, investment_mix, profits, node, wallet, investment, profit_obtained
+    investment_earnings = []
+    shares_purchased = []
+    all_investments = []
+    obtain_possible_investment(
+        names,
+        costs,
+        profits,
+        node,
+        wallet,
+        profit_obtained,
+        investment_earnings,
+        shares_purchased,
+        all_investments,
     )
-    return investment_mix, profits
+    return printable_result(investment_earnings, all_investments)
