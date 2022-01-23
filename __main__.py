ad68c88fe77dc4e6eb600c5a471c17b95d048e2b
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-01-23 18:28:07 +0100

    Popup windows for open csv

--- __main__.py
+++ __main__.py
@@ -1,83 +1,59 @@
-import time
+from __future__ import annotations
+
 from algorithms.bruteforce import bruteforce
 from algorithms.optimized import optimized
-from utils import get_shares
-import tracemalloc
-
-max_expenses = 500
-csvfile = 'dataset1.csv'
-names, costs, profits = get_shares(csvfile,20)
-
-print("\n--- Bruteforce (20) ---")
-start_time = time.time()
-tracemalloc.start()
-result = bruteforce(names,costs,profits,max_expenses)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end_time = time.time() - start_time
-print(result)
-print("\x1b[36m--- Résultat obtenu en %s seconds ---" % (end_time))
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB\x1b[0m")
+from utils import get_shares, open_csv, write_results
 
 
-print("\n--- Optimized (20) ---")
-start_time = time.time()
-tracemalloc.start()
-result = optimized(names,costs,profits,max_expenses)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end_time = time.time() - start_time
-print(result)
-print("\x1b[36m--- Résultat obtenu en %s seconds ---" % (end_time))
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB\x1b[0m")
+def run_with_comparision(
+    max_expenses: int, names: list[str], costs: list[float], profits: list[float]
+) -> None:
+    """Compare execution time and memory consumption with bruteforce and optimized algorithms"""
+    import time
+    import tracemalloc
+
+    print("\n--- Bruteforce ---")
+    start_time = time.time()
+    tracemalloc.start()
+    result = bruteforce(names, costs, profits, max_expenses)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    end_time = time.time() - start_time
+    print(result)
+    print("\x1b[36m--- Résultat obtenu en %s seconds ---" % (end_time))
+    print(
+        f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB\x1b[0m"
+    )
+
+    print("\n--- Optimized ---")
+    start_time = time.time()
+    tracemalloc.start()
+    result = optimized(names, costs, profits, max_expenses)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    end_time = time.time() - start_time
+    print(result)
+    print("\x1b[36m--- Résultat obtenu en %s seconds ---" % (end_time))
+    print(
+        f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB\x1b[0m"
+    )
 
 
-names, costs, profits = get_shares(csvfile,25)
+if __name__ == "__main__":
+    import os
 
-print("\n--- Bruteforce (25) ---")
-start_time = time.time()
-tracemalloc.start()
-result = bruteforce(names,costs,profits,max_expenses)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end_time = time.time() - start_time
-print(result)
-print("--- Résultat obtenu en %s seconds ---" % (end_time))
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+    os.system("cls")
+    max_expenses = 500
+    input(
+        """Maximum expense set at 500€.
+Press enter and choose a file which contains shares name, cost and profit."""
+    )
+    csvfile = open_csv()
 
+    names, costs, profits = get_shares(csvfile, max_expenses, 20)
+    result = optimized(names, costs, profits, max_expenses)
 
-print("\n--- Optimized (25) ---")
-start_time = time.time()
-tracemalloc.start()
-result = optimized(names,costs,profits,max_expenses)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end_time = time.time() - start_time
-print(result)
-print("--- Résultat obtenu en %s seconds ---" % (end_time))
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+    textfile = csvfile[:-4] + "_results.txt"
+    write_results(textfile, result)
 
-names, costs, profits = get_shares(csvfile,30)
-
-print("\n--- Bruteforce (30) ---")
-start_time = time.time()
-tracemalloc.start()
-result = bruteforce(names,costs,profits,max_expenses)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end_time = time.time() - start_time
-print(result)
-print("--- Résultat obtenu en %s seconds ---" % (end_time))
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
-
-
-print("\n--- Optimized (30) ---")
-start_time = time.time()
-tracemalloc.start()
-result = optimized(names,costs,profits,max_expenses)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-end_time = time.time() - start_time
-print(result)
-print("--- Résultat obtenu en %s seconds ---" % (end_time))
-print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+    print(result, f"\nResult export in {textfile}")
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
-import csv
-import gc
 
-def get_data(csvfile: str) -> list[tuple[str, float, float]]:
+import csv
+from typing import Optional
+
+
+def get_data(csvfile: str, max_expenses: int) -> list[tuple[str, float, float]]:
     """
     Return three list that contains the name of a stock, its price and its profit percentage.
     The first line of the csv file is ignored.
@@ -12,31 +14,62 @@ def get_data(csvfile: str) -> list[tuple[str, float, float]]:
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            if len(row[0])>0 and float(row[1]) > 0 and float(row[2]) > 0:
-                names.append(row[0])
-                costs.append(float(row[1]))
-                profits.append(float(row[2]))
+            name = row[0]
+            cost = float(row[1])
+            profit = float(row[2])
+            if len(name) > 0 and cost > 0 and cost <= max_expenses and profit > 0:
+                names.append(name)
+                costs.append(cost)
+                profits.append(profit)
     return names, costs, profits
 
 
-def extract_best_shares(names: list[str], costs: list[float], profits: list[float], nb_shares: int=-1) -> tuple[list[str], list[float], list[float]]:
+def extract_best_shares(
+    names: list[str],
+    costs: list[float],
+    profits: list[float],
+    nb_shares: Optional[int] = None,
+) -> tuple[list[str], list[float], list[float]]:
     """
     After having zipped the three lists provided as input, we sort by decreasing order of profits
     and return them by keeping only a certain number of elements
     """
-    if nb_shares<len(names) and nb_shares>0:
+    if (
+        nb_shares is not None
+        and isinstance(nb_shares, int)
+        and nb_shares < len(names)
+        and nb_shares > 0
+    ):
         zipped = zip(profits, costs, names)
         sorted_desc = sorted(zipped, reverse=True)
         profits, costs, names = [list(elt)[:nb_shares] for elt in zip(*sorted_desc)]
     return names, costs, profits
 
 
-def get_shares(csvfile:str, nb_shares:int=-1):
+def get_shares(csvfile: str, max_expenses: int, nb_shares: Optional[int] = None):
     """
     We recover from a csv a number of actions returned in the form of three lists (names, costs, profits)
     We free the non-referenced memory
     """
-    names, costs, profits = get_data(csvfile)
+    names, costs, profits = get_data(csvfile, max_expenses)
     names, costs, profits = extract_best_shares(names, costs, profits, nb_shares)
-    gc.collect()
     return names, costs, profits
+
+
+def write_results(textfile: str, text_to_write: str) -> None:
+    """Write investment result in a file"""
+    f = open(textfile, "w")
+    f.write(text_to_write)
+    f.close()
+
+
+def open_csv():
+    """Window (GUI) for open a file"""
+    import tkinter as tk
+    from tkinter import filedialog
+
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    return file_path
